b31e9814fef85c413195c7e117d9b510275d65ea
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-12 23:44:26 -0500

    Add sparkline image and download

--- lib/sparkline_web/controllers/input_controller.ex
+++ lib/sparkline_web/controllers/input_controller.ex
@@ -11,9 +11,33 @@ defmodule SparklineWeb.InputController do
 
     # Reads file and stores as string
     text = elem(File.read(path), 1)
+
+    # Turns file into list of ints
     nums = Enum.map(String.split(text, ", ", trim: true), fn x -> elem(Integer.parse(x), 0) end)
-    IO.inspect nums
-    render(conn, "input_veiw.html", [changeset: :changeset, action: "", file_path: path])
+
+    # Finds max and min
+    [head | _tail] = nums
+    {min, max} = Enum.reduce(nums, {head, head}, fn(num, {min, max}) ->
+      {Enum.min([num, min]), Enum.max([num, max])}
+    end)
+
+    # Calculates image details
+    height = 23
+    width = 189
+    step = width / (length(nums) - 1)
+    reversedNums = Enum.map(nums, fn x -> -x end)
+
+    # Calculates image points
+    # 0, 0 is top left
+    # increasing x and y go to bottom right
+    shiftedNums = Enum.map(reversedNums, fn x -> x + max end)
+    scale = (max - min) / height
+    scaledNums = Enum.map(shiftedNums, fn x -> x / scale end)
+    points = scaledNums
+      |> Enum.with_index
+      |> Enum.map(fn{y, index} -> [index * step, y] end)
+
+    render(conn, "input_veiw.html", [changeset: :changeset, action: "", points: points, height: height, width: width])
   end
 
 end
