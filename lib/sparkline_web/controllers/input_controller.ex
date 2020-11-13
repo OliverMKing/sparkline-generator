6f9e8971fe89a4c8baabf4e92053e4c1677b451c
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-12 21:07:07 -0500

    Add file parsing to controller

--- lib/sparkline_web/controllers/input_controller.ex
+++ lib/sparkline_web/controllers/input_controller.ex
@@ -8,6 +8,11 @@ defmodule SparklineWeb.InputController do
   def post(conn, %{"changeset" => changeset}) do
     %{"in_file" => in_file} = changeset
     path = Path.expand(in_file.path)
+
+    # Reads file and stores as string
+    text = elem(File.read(path), 1)
+    nums = Enum.map(String.split(text, ", ", trim: true), fn x -> elem(Integer.parse(x), 0) end)
+    IO.inspect nums
     render(conn, "input_veiw.html", [changeset: :changeset, action: "", file_path: path])
   end
 
