0170dca0ce8f52b4770dc05d4c042035c606071c
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-22 21:59:54 -0500

    Add error handling for incorrect file type

--- lib/sparkline_web/controllers/input_controller.ex
+++ lib/sparkline_web/controllers/input_controller.ex
@@ -2,42 +2,67 @@ defmodule SparklineWeb.InputController do
   use SparklineWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "input.html", [changeset: :changeset, action: ""])
+    render(conn, "input.html", changeset: :changeset, action: "")
   end
 
+  @spec post(Plug.Conn.t(), map) :: Plug.Conn.t()
   def post(conn, %{"changeset" => changeset}) do
     %{"in_file" => in_file} = changeset
     path = Path.expand(in_file.path)
 
-    # Reads file and stores as string
-    text = elem(File.read(path), 1)
+    try do
+      # Reads file and stores as string
+      case File.read(path) do
+        {:error, _} ->
+          exit("Incorrect filetype")
 
-    # Turns file into list of ints
-    nums = Enum.map(String.split(text, ", ", trim: true), fn x -> elem(Integer.parse(x), 0) end)
+        {:ok, text} ->
+          # Turns file into list of ints
+          try do
+            nums =
+              Enum.map(String.split(text, ", ", trim: true), fn x -> elem(Integer.parse(x), 0) end)
 
-    # Finds max and min
-    [head | _tail] = nums
-    {min, max} = Enum.reduce(nums, {head, head}, fn(num, {min, max}) ->
-      {Enum.min([num, min]), Enum.max([num, max])}
-    end)
+            # Finds max and min
+            [head | _tail] = nums
 
-    # Calculates image details
-    height = 23
-    width = 189
-    step = width / (length(nums) - 1)
-    reversedNums = Enum.map(nums, fn x -> -x end)
+            {min, max} =
+              Enum.reduce(nums, {head, head}, fn num, {min, max} ->
+                {Enum.min([num, min]), Enum.max([num, max])}
+              end)
 
-    # Calculates image points
-    # 0, 0 is top left
-    # increasing x and y go to bottom right
-    shiftedNums = Enum.map(reversedNums, fn x -> x + max end)
-    scale = (max - min) / height
-    scaledNums = Enum.map(shiftedNums, fn x -> x / scale end)
-    points = scaledNums
-      |> Enum.with_index
-      |> Enum.map(fn{y, index} -> [index * step, y] end)
+            # Calculates image details
+            height = 23
+            width = 189
+            step = width / (length(nums) - 1)
+            reversedNums = Enum.map(nums, fn x -> -x end)
 
-    render(conn, "input_veiw.html", [changeset: :changeset, action: "", points: points, height: height, width: width])
+            # Calculates image points
+            # 0, 0 is top left
+            # increasing x and y go to bottom right
+            shiftedNums = Enum.map(reversedNums, fn x -> x + max end)
+            scale = (max - min) / height
+            scaledNums = Enum.map(shiftedNums, fn x -> x / scale end)
+
+            points =
+              scaledNums
+              |> Enum.with_index()
+              |> Enum.map(fn {y, index} -> [index * step, y] end)
+
+            render(conn, "input_view.html",
+              changeset: :changeset,
+              action: "",
+              points: points,
+              height: height,
+              width: width
+            )
+          rescue
+            # Error if file can't parse
+            _ -> render(conn, "input_error.html")
+          end
+      end
+    catch
+      # Error if filetype is wrong
+      :exit -> render(conn, "input_error.html")
+    end
   end
-
 end
